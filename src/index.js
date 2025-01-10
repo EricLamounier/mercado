a5b35d0474bdb2bf3125ef61a5da91d73a3338fd
EricLamounier <eric_lamounier@outlook.com>
2025-01-09 22:49:44 -0300

    aiai

--- src/index.js
+++ src/index.js
@@ -17,17 +17,6 @@ root.render(
     <App />
 );
 
-//serviceWorkerRegistration.register();
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch((error) => {
-        console.log('Service Worker registration failed:', error);
-      });
-  });
-}
+serviceWorkerRegistration.register();
 
 reportWebVitals();
